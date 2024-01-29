5ba6f4e99ccd196c62a9b3a29143caa560f30c73
Egor.Bogachev <Egor.Bogachev@N-BOGACHEVEP>
2024-01-29 14:31:13 +0300

    Добавьте файлы проекта.

---
+++ Test_app_1/Test_app_1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+
+namespace Test_app_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            WebView webView = new WebView
+            {
+                Source = "Map_API.html",
+                VerticalOptions = LayoutOptions.StartAndExpand
+            };
+
+        }
+
+        private async void ButtonClick(object sender, EventArgs e)
+        {
+            redirText.Text = "Redirecting other page";
+            await DisplayAlert("Redirect", "Redirecting", "cancel");
+        }
+
+        
+    }
+}
