95625d1b89df57a40e21c49cc4363bcbc1b10f4b
EgorTW <eg.bogachev2014@yandex.ru>
2024-01-30 16:59:22 +0300

    Update with switching pages

--- Test_app_1/Test_app_1/MainPage.xaml.cs
+++ Test_app_1/Test_app_1/MainPage.xaml.cs
@@ -5,12 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using System.Net.Http;
 
 namespace Test_app_1
 {
     public partial class MainPage : ContentPage
     {
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,12 +23,38 @@ namespace Test_app_1
 
         }
 
+        public static class PageDictionary
+        {
+            public static Dictionary<string, Type> PageTypes = new Dictionary<string, Type>
+            {
+                {"MainPage", typeof(MainPage)},
+                {"QA", typeof(QA_page)},
+            };
+        }
         private async void ButtonClick(object sender, EventArgs e)
         {
             redirText.Text = "Redirecting other page";
             await DisplayAlert("Redirect", "Redirecting", "cancel");
         }
 
-        
+        private async void switchPage(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonName = button.Text;
+
+            if(PageDictionary.PageTypes.ContainsKey(buttonName))
+            {
+                var pageType = PageDictionary.PageTypes[buttonName];
+                var page = (ContentPage)Activator.CreateInstance(pageType);
+
+                await Navigation.PushAsync(page);
+            }
+            else
+            {
+                await DisplayAlert("Error", "Page not found", "OK");
+            }
+        }
     }
+
+
 }
